bd990d5ba61009df695406bdc90672ca2fd9a74b
Sima Margulis Davidi <simamarg@gmail.com>
2018-02-22 18:07:44 +0200

    minor changes

--- routes/db.js
+++ routes/db.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
  var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '12345',
+  password : '1234',
   database : 'catscrm'
 });
 
